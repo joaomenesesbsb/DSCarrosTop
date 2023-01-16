ad74b8a427542709b2e09f7c575a3e9f4e980950
Joao <joaovic2012@hotmail.com>
2023-01-16 11:35:00 -0300

    Card catalog

---
+++ src/components/CardCatalog/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function CardCatalog(){
+    return (
+        <>
+        <div className='card-catalog-ct'>
+            <img src="../../assets/car.png" alt="Car" />
+            <h3>Lorem ipsum dolor</h3>
+        </div>
+        </>
+    )
+}
--- src/routes/catalog/index.tsx
+++ src/routes/catalog/index.tsx
@@ -1,14 +1,17 @@
 import './styles.css';
 
 import HeaderCT from '../../components/HeaderCT';
+import CardCatalog from '../../components/CardCatalog';
 
 export default function CatalogCT(){
     return(
         <>
         <HeaderCT />
         <main>
-            <section>
-
+            <section id='section-catalog-content' className='container'>
+                <h2>Venha nos visitar</h2>
+                <CardCatalog />
+                <CardCatalog />
             </section>
             <section>
                 
